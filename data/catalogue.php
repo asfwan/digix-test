f4f05fa75a94f2061804eff8998e04a3ff6f68a1
Ahmad Saufwan bin Sazali <asfwan@gmail.com>
2017-08-04 11:46:56 +0800

    created catalogue of items

---
+++ data/catalogue.php
@@ -0,0 +1,30 @@
+<?php
+	
+return [
+	
+	// the products catalogue
+	'ipd' => new Item([
+		'price'=>549.99,
+		'name'=>'Super iPad',
+		'sku'=>'ipd'
+	]),
+	
+	'mbp' => new Item([
+		'price'=>1399.99,
+		'name'=>'Macbook Pro',
+		'sku'=>'mbp'
+	]),
+	
+	'atv' => new Item([
+		'price'=>109.50,
+		'name'=>'Apple TV',
+		'sku'=>'atv'
+	]),
+	
+	'vga' => new Item([
+		'price'=>30.00,
+		'name'=>'VGA Adapter',
+		'sku'=>'vga'
+	]),
+
+];
